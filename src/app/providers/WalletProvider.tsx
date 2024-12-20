6b2283132f15010b088468602bef64fc369b28d4
Vladimir Jovanovic <vjovanovic@showingtime.com>
2024-12-20 04:47:04 +0100

    Change import type

--- src/app/providers/WalletProvider.tsx
+++ src/app/providers/WalletProvider.tsx
@@ -7,7 +7,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { clusterApiUrl } from '@solana/web3.js';
 
 // Import wallet adapter styles
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 interface Props {
     children: ReactNode;
